55440c8cd3e9eb8cd810168e4e27a73d3f45b7fe
Noel O'Boyle <baoilleach@gmail.com>
2009-12-17 12:44:49 +0000

    Speed things by using regular expressions. Slow things down by doing a more thorough search for the "CanonicalSMARTS" in the case of multiple ring systems.

--- c++/fragments/removedups.py
+++ c++/fragments/removedups.py
@@ -1,3 +1,4 @@
+import re
 import os
 import sys
 import pybel
@@ -6,29 +7,28 @@ conv = pybel.ob.OBConversion()
 conv.SetOutFormat("smi")
 conv.SetOptions("C", conv.OUTOPTIONS)
 
-elems = ['Cl', 'Se', 'Br', 'Al', 'Si', 'As', 'Li', 'Na',
-         'Mg', 'Ca', 'Cr', 'Mn', 'Fe', 'Co', 'Ni', 'Zn',
-         'Cu', 'B', 'C', 'N', 'O', 'F', 'P', 'S', 'I', 'K']
-aroms = ['si', 'se', 'b', 'c', 'n', 'o', 'p', 's']
+##elems = ['Cl', 'Se', 'Br', 'Al', 'Si', 'As', 'Li', 'Na',
+##         'Mg', 'Ca', 'Cr', 'Mn', 'Fe', 'Co', 'Ni', 'Zn',
+##         'Cu', 'B', 'C', 'N', 'O', 'F', 'P', 'S', 'I', 'K']
+##aroms = ['si', 'se', 'b', 'c', 'n', 'o', 'p', 's']
 
-data = ((elems, 'A'), (aroms, 'a'))
+def SMILEStoSMARTS(smile):
+    """
+    >>> SMILEStoSMARTS("CC[Cl]nnn[si][Se]")
+    'AAAaaaaA'
+    """
+    newname = re.sub("\[(Cl|Se|Br|Al|Si|As|Li|Na|Mg|Ca|Cr|Mn|Fe|Co|Ni|Zn|Cu)\]|[BCNOFPSIK]", "A", smile)
+    newname = re.sub("\[(si|se)\]|[bcnops]", "a", newname)
+    return newname    
 
 def CanonicalSMARTS(smiles):
     mol = pybel.readstring("smi", smiles)
-    names = [conv.WriteString(mol.OBMol, True) for i in range(100)]
-
-    smarts = []
-    for name in names:
-        newname = name
-        for elems, smart in data:
-            for elem in elems:
-                if len(elem) == 2:
-                    txt = "[%s]" % elem
-                else:
-                    txt = elem
-                newname = newname.replace(txt, smart)
-        smarts.append(newname)
-
+    # Make sure you do at least 100, and then add in an extra 100 for each
+    # ring
+    N = 100 * len(mol.OBMol.GetSSSR())
+    names = [conv.WriteString(mol.OBMol, True) for i in range(N)]
+    unames = set(names)
+    smarts = [SMILEStoSMARTS(name) for name in unames]
     return min(smarts)
 
 def usage():
@@ -37,17 +37,30 @@ def usage():
 Remove duplicate SMARTS strings from freq-sort.txt to create
 freq-nodups.txt.
 """
-        
-if __name__ == "__main__":
-    if len(sys.argv) != 3 or not os.path.isfile(sys.argv[1]) or not os.path.isfile(sys.argv[2]):
-        usage()
-        sys.exit(1)
-        
 
-    fragfile = sys.argv[1]
-    freqfile = sys.argv[2]
+def analyse(fragfile, freqfile):
+    """Get some idea of how many possible SMARTS representations
+    there are for a particular molecule"""
+    xyz = {}
+    for line in open(fragfile, "r"):
+        broken = line.split()
+        if len(broken) == 4 and line[0] != 'C':
+            xyz["\t".join(broken[:3])] = broken[-1].rstrip()
 
-    
+    N = 10000
+    seen = set()
+    for line in open(freqfile, "r"):
+        broken = line.split()
+        lookup = "\t".join(broken[2:])
+        assert lookup in xyz
+        mol = pybel.readstring("smi", xyz[lookup])
+        names = set([conv.WriteString(mol.OBMol, True) for i in range(N)])
+        smarts = [SMILEStoSMARTS(name) for name in names]
+        usmarts = set(smarts)
+
+        print xyz[lookup], len(names), len(usmarts)
+
+def main(fragfile, freqfile):
     xyz = {}
     for line in open(fragfile, "r"):
         broken = line.split()
@@ -63,5 +76,22 @@ if __name__ == "__main__":
         if cansmarts not in seen:
             print line.rstrip()
             seen.add(cansmarts)
+
+def test():
+    import doctest
+    doctest.testmod()
         
+if __name__ == "__main__":
+    if len(sys.argv) != 3 or not os.path.isfile(sys.argv[1]) or not os.path.isfile(sys.argv[2]):
+        usage()
+        sys.exit(1)
         
+    fragfile = sys.argv[1]
+    freqfile = sys.argv[2]
+
+    main(fragfile, freqfile)
+##    analyse(fragfile, freqfile)
+##    test()
+
+        
+
